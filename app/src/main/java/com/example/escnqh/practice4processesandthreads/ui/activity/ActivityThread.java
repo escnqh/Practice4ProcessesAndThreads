17b78be79d5effca0ed67075d378c86e9eb99afd
Escnqh <844071773@qq.com>
2018-04-13 17:45:23 +0800

    init this respositories

---
+++ app/src/main/java/com/example/escnqh/practice4processesandthreads/ui/activity/ActivityThread.java
@@ -0,0 +1,19 @@
+package com.example.escnqh.practice4processesandthreads.ui.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityThread extends AppCompatActivity {
+
+
+    private static String TAG = ActivityThread.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
